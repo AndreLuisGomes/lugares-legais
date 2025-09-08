dd185ce82bb79a2606820dd4a5cfcaa239a37144
AndreLuisGomes <andreluisgomes09@gmail.com>
2025-09-07 21:34:48 -0300

    feat: adicionando novas funcionalidades

--- src/app/lugares/lugar/lugar.component.ts
+++ src/app/lugares/lugar/lugar.component.ts
@@ -20,7 +20,7 @@ export class LugarComponent implements OnInit {
   ) {
     this.camposForm = new FormGroup({
       nome: new FormControl('', Validators.required),
-      categoria: new FormControl('', Validators.required),
+      categoria: new FormControl(null, Validators.required),
       localizacao: new FormControl('', Validators.required),
       urlFoto: new FormControl('', Validators.required),
       avaliacao: new FormControl(null, Validators.required),
